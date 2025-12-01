708ca35ce2cb69ece46ed737b8cb3f4629190271
Amanda Grip <amanda.grip@stud.sti.se>
2025-12-01 13:27:28 +0100

    created todo class
    used @ManytoOne to connect many todos to one user

--- src/main/java/com/example/to_do_application/entities/Todo.java
+++ src/main/java/com/example/to_do_application/entities/Todo.java
@@ -1,4 +1,47 @@
 package com.example.to_do_application.entities;
 
+import jakarta.persistence.*;
+
+@Entity
 public class Todo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    private boolean completed = false;
+
+    @ManyToOne
+    @JoinColumn(name = "owner_id")
+    private User owner;
+
+    public Todo(){}
+
+    public long getId() {
+        return id;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public boolean isCompleted() {
+        return completed;
+    }
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+    public User getOwner() {
+        return owner;
+    }
+    public void setOwner(User owner) {
+        this.owner = owner;
+    }
 }
