b12912b5f0d856943add167ad40008ec1ad8e0d1
Amanda Grip <amanda.grip@stud.sti.se>
2025-12-01 13:12:12 +0100

    created user and role class
    imported @Entity for both classes and also columns

--- src/main/java/com/example/to_do_application/entities/User.java
+++ src/main/java/com/example/to_do_application/entities/User.java
@@ -1,4 +1,63 @@
 package com.example.to_do_application.entities;
 
+import jakarta.persistence.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String userName;
+    private String password;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+
+    private Set<Role> roles = new HashSet<>();
+
+    @OneToMany(mappedBy = "owner", cascade = CascadeType.ALL)
+    private Set<Todo> todos = new HashSet<>();
+
+    public User() {
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getUserName() {
+        return userName;
+    }
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public Set<Role> getRoles() {
+        return roles;
+    }
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
+    }
+    public Set<Todo> getTodos() {
+        return todos;
+    }
+    public void setTodos(Set<Todo> todos) {
+        this.todos = todos;
+    }
 }
